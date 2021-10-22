ddfccfb94f6c3d41f6f65cf43d3b5ef56378975f
AlexYach <Alexyach1973@gmail.com>
2021-10-22 17:40:24 +0300

    LabWork_2-1-7

---
+++ app/src/main/java/com/example/android/labwork_2_1_7/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.labwork_2_1_7;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                textView.setText("Add");
+                return true;
+            case R.id.create:
+                textView.setText("Create");
+                return true;
+            case R.id.download:
+                textView.setText("Download");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
